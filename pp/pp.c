f9c9473ed80eaa2e083b006ddac9df7256cd9350
Mahjon <budd123@live.com>
2021-06-27 15:42:26 +0800

    My first commit

---
+++ pp/pp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+char *ps = {"We are the world"};
+int main(void)
+{
+    int i = 1;
+    int j = 2;
+    printf("%d\n",i*j);
+    printf("Hello\n");
+    return 0;
+}
